0881bb1348af3fc25337a9fcc83ade5a3851f360
Kenaz Mathukutty <kenazmathukutty2028@cs.sjcetpalai.ac.in>
2025-07-22 18:44:20 +0530

    gitattributes.java

--- gitattributes.java
+++ gitattributes.java
@@ -1,29 +1,21 @@
 public class Main {
-    // Define the Student class inside the same file
-    static class Student {
-        String name;
-        int rollNumber;
-        char grade;
-
-        // Method to display student details
-        public void displayDetails() {
-            System.out.println("Name: " + name);
-            System.out.println("Roll Number: " + rollNumber);
-            System.out.println("Grade: " + grade);
-        }
-    }
-
     public static void main(String[] args) {
-        // Create a Student object
-        Student student1 = new Student();
+        Student student = new Student();
+        student.name = "Alice";
+        student.rollNumber = 101;
+        student.grade = 'A';
 
-        // Assign sample data
-        student1.name = "Alice";
-        student1.rollNumber = 101;
-        student1.grade = 'A';
-
-        // Call displayDetails() method
-        student1.displayDetails();
+        student.displayDetails();
     }
 }
+class Student {
+    String name;
+    int rollNumber;
+    char grade;
 
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("Roll Number: " + rollNumber);
+        System.out.println("Grade: " + grade);
+    }
+}
